d1a860615a1648389dfdee7f893fd6590a720189
Марлен Каримов <kurukurueer0@gmail.com>
2026-03-11 16:45:54 +0600

    add actions, lint

--- app.go
+++ app.go
@@ -109,6 +109,11 @@ func IsNil(val any) bool {
 	switch v.Kind() {
 	case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice:
 		return v.IsNil()
+	case reflect.Invalid, reflect.Bool, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32,
+		reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Uintptr, reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128,
+		reflect.Array, reflect.String, reflect.Struct, reflect.UnsafePointer:
+		return false
 	default:
 		return false
 	}
